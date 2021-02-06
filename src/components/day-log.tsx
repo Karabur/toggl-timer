5d5602d23e7bbaf1f22e2698e5cc223a2891b2d6
Anton Ulyanov <anton.ulianov@gmail.com>
2021-02-06 18:52:09 +0700

    add daylog hours list and current time indicator

--- src/components/day-log.tsx
+++ src/components/day-log.tsx
@@ -1,12 +1,45 @@
 import { observer } from 'mobx-react-lite'
 import { Typography } from 'antd'
+import styles from './day-log.module.sass'
+import { useEffect, useState } from 'react'
+import { getHours, getMinutes, getSeconds } from 'date-fns'
+import classnames from 'classnames'
+
+const hours = Array.from(Array(24)).map((v, i) => i)
+const hoursStr = hours.map((hour) => `${hour < 10 ? '0' : ''}${hour}:00`)
 
 function DayLog() {
+	const [curTime, setCurTime] = useState(Date.now())
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setCurTime(Date.now)
+		}, 1000)
+		return () => clearInterval(timer)
+	}, [])
+
+	const curHour = getHours(curTime)
+	const currentTimeOffset = `${((getMinutes(curTime) * 60 + getSeconds(curTime)) / 3600) * 100}%`
+
 	return (
 		<div>
 			<Typography.Title level={3} className={'centered'}>
 				Today
 			</Typography.Title>
+			<div className={styles.list}>
+				{hours.map((hour) => (
+					<div className={styles.line}>
+						<div className={styles.hour}>{hoursStr[hour]}</div>
+						<div className={classnames(styles.hourBox, { [styles.active]: curHour === hour })}>
+							{curHour === hour ? (
+								<div className={styles.currentTimeMark} style={{ left: currentTimeOffset }} />
+							) : (
+								false
+							)}
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	)
 }
